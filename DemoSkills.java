290df50cbbbbd52d926cd3abf5e55b100a2554cf
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2022-05-04 18:40:38 -0700

    test1

--- DemoSkills.java
+++ DemoSkills.java
@@ -1,1 +1,12 @@
+public class DemoSkills{
+    int age;
+    int height;
+    public DemoSkills(int age, int height){
+        this.age = age;
+        this.height = height;
+    }
 
+    public int average(){
+        return height/age;
+    }
+}
--- DemoSkillsTest.java
+++ DemoSkillsTest.java
@@ -1,1 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 
+public class DemoSkillsTest{
+    DemoSkills test1 = new DemoSkills(2, 60);
+
+    @Test
+    public void Test(){
+        assertEquals(20, test1.average());
+    }
+}
